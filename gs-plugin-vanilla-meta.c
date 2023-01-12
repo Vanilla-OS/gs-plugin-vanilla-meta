34141c0ffeb2fcac1130889242175835fd7ff7c4
Mateus Melchiades <mateusbmelchiades@gmail.com>
2023-01-12 20:09:11 -0300

    Use own worker for downloading metadata file

--- gs-plugin-vanilla-meta.c
+++ gs-plugin-vanilla-meta.c
@@ -8,24 +8,59 @@
 
 #include "gs-plugin-vanilla-meta.h"
 
-static void progress_cb(gsize bytes_downloaded, gsize total_download_size, gpointer user_data);
-static void download_file_cb(GObject *source_object, GAsyncResult *result, gpointer user_data);
+static void refresh_metadata_thread_cb(GTask *task,
+                                       gpointer source_object,
+                                       gpointer task_data,
+                                       GCancellable *cancellable);
 static gboolean add_apps_from_metadata_file(GsPluginVanillaMeta *self,
                                             GFile *metadata_file,
                                             GCancellable *cancellable,
                                             GError **error);
 
 const gchar *gz_metadata_filename   = ".cache/vanilla_meta/metadata.xml.gz";
-const gchar *metadata_filename      = ".cache/vanilla_meta/metadata.xml";
 const gchar *metadata_silo_filename = ".cache/vanilla_meta/metadata.xmlb";
 const gchar *metadata_url           = "";
 
 struct _GsPluginVanillaMeta {
     GsPlugin parent;
+    GsWorkerThread *worker; /* (owned) */
     XbSilo *silo;
 };
 
 G_DEFINE_TYPE(GsPluginVanillaMeta, gs_plugin_vanilla_meta, GS_TYPE_PLUGIN)
+
+static void
+gs_plugin_vanilla_meta_dispose(GObject *object)
+{
+    GsPluginVanillaMeta *self = GS_PLUGIN_VANILLA_META(object);
+
+    g_clear_object(&self->worker);
+    G_OBJECT_CLASS(gs_plugin_vanilla_meta_parent_class)->dispose(object);
+}
+
+static void
+gs_plugin_vanilla_meta_setup_async(GsPlugin *plugin,
+                                   GCancellable *cancellable,
+                                   GAsyncReadyCallback callback,
+                                   gpointer user_data)
+{
+    GsPluginVanillaMeta *self = GS_PLUGIN_VANILLA_META(plugin);
+    g_autoptr(GTask) task     = NULL;
+
+    task = g_task_new(plugin, cancellable, callback, user_data);
+    g_task_set_source_tag(task, gs_plugin_vanilla_meta_setup_async);
+
+    // Start up a worker thread to process all the plugin’s function calls.
+    self->worker = gs_worker_thread_new("gs-plugin-vanilla-meta");
+
+    g_task_return_boolean(task, true);
+}
+
+static gboolean
+gs_plugin_vanilla_meta_setup_finish(GsPlugin *plugin, GAsyncResult *result, GError **error)
+{
+    return g_task_propagate_boolean(G_TASK(result), error);
+}
 
 static void
 gs_plugin_vanilla_meta_init(GsPluginVanillaMeta *self)
@@ -52,104 +87,60 @@ gs_plugin_vanilla_meta_refresh_metadata_async(GsPlugin *plugin,
                                               GAsyncReadyCallback callback,
                                               gpointer user_data)
 {
-    g_autoptr(GFile) gz_file            = g_file_new_for_path(gz_metadata_filename);
-    g_autoptr(GTask) task               = NULL;
-    g_autoptr(SoupSession) soup_session = NULL;
-    g_autoptr(GError) error             = NULL;
+    GsPluginVanillaMeta *self = GS_PLUGIN_VANILLA_META(plugin);
+    g_autoptr(GTask) task     = NULL;
 
     task = g_task_new(plugin, cancellable, callback, user_data);
     g_task_set_source_tag(task, gs_plugin_vanilla_meta_refresh_metadata_async);
+    g_task_set_task_data(task, gs_plugin_refresh_metadata_data_new(cache_age_secs, flags),
+                         (GDestroyNotify)gs_plugin_refresh_metadata_data_free);
+
+    gs_worker_thread_queue(self->worker, G_PRIORITY_LOW, refresh_metadata_thread_cb,
+                           g_steal_pointer(&task));
+}
+
+static void
+refresh_metadata_thread_cb(GTask *task,
+                           gpointer source_object,
+                           gpointer task_data,
+                           GCancellable *cancellable)
+{
+    GsPluginVanillaMeta *self         = GS_PLUGIN_VANILLA_META(source_object);
+    GsPluginRefreshMetadataData *data = task_data;
+
+    g_autoptr(GError) error          = NULL;
+    g_autoptr(GFile) f               = g_file_new_for_uri(metadata_url);
+    g_autoptr(GFileInputStream) f_is = NULL;
+
+    g_autoptr(GFile) gz_file            = g_file_new_for_path(gz_metadata_filename);
+    g_autoptr(SoupSession) soup_session = NULL;
 
     soup_session = gs_build_soup_session();
 
     // Is the metadata missing or too old?
-    if (gs_utils_get_file_age(gz_file) >= cache_age_secs) {
+    if (gs_utils_get_file_age(gz_file) >= data->cache_age_secs) {
         g_debug("I should refresh metadata");
 
-        gs_download_file_async(soup_session, metadata_url, gz_file, G_PRIORITY_LOW, progress_cb,
-                               plugin, cancellable, download_file_cb, g_steal_pointer(&task));
+        // Download metadata file
+        f_is = g_file_read(f, cancellable, &error);
+        if (error != NULL) {
+            g_debug("Could not open input stream for uri: %s", error->message);
+            g_task_return_error(task, g_steal_pointer(&error));
+            return;
+        }
 
-        return;
+        if (!g_file_copy(f, gz_file, G_FILE_COPY_OVERWRITE, cancellable, NULL, NULL, &error)) {
+            g_debug("Could not copy file to disk: %s", error->message);
+            g_task_return_error(task, g_steal_pointer(&error));
+            return;
+        }
     } else {
         g_debug("Cache is only %zu seconds old, https packets aren't free, ya know?",
                 gs_utils_get_file_age(gz_file));
-
-        add_apps_from_metadata_file(GS_PLUGIN_VANILLA_META(plugin), gz_file, cancellable, &error);
     }
 
+    add_apps_from_metadata_file(self, gz_file, cancellable, &error);
     g_task_return_boolean(task, TRUE);
-}
-
-static void
-progress_cb(gsize bytes_downloaded, gsize total_download_size, gpointer user_data)
-{
-    g_debug("Downloaded %zu of %zu bytes", bytes_downloaded, total_download_size);
-}
-
-static void
-download_file_cb(GObject *source_object, GAsyncResult *result, gpointer user_data)
-{
-    g_autoptr(GTask) task         = g_steal_pointer(&user_data);
-    GsPluginVanillaMeta *self     = GS_PLUGIN_VANILLA_META(g_task_get_source_object(task));
-    g_autoptr(GError) local_error = NULL;
-    SoupSession *soup_session     = SOUP_SESSION(source_object);
-    GCancellable *cancellable     = g_task_get_cancellable(task);
-
-    g_autoptr(GFile) gz_file                        = g_file_new_for_path(gz_metadata_filename);
-    g_autoptr(GFile) xml_file                       = NULL;
-    g_autoptr(GZlibDecompressor) decompressor       = NULL;
-    g_autoptr(GError) error                         = NULL;
-    g_autoptr(GFileInputStream) gz_file_inputstream = NULL;
-    g_autoptr(GFileIOStream) xml_file_iostream      = NULL;
-    g_autoptr(GOutputStream) xml_outputstream       = NULL;
-    g_autoptr(GInputStream) stream_data             = NULL;
-
-    if (!gs_download_file_finish(soup_session, result, &local_error)) {
-        g_debug("Error while downloading metadata: %s", local_error->message);
-        g_task_return_error(task, g_steal_pointer(&local_error));
-    } else {
-        g_debug("Successfully downloaded new metadata");
-
-        // NOTE: Maybe we won't even need to do this, as xmlb can load from a compressed xml
-        // Such hard work, FOR NOTHING!!!
-        g_debug("Uncompressing metadata");
-        // Uncompress gzip into xml metadata
-        decompressor        = g_zlib_decompressor_new(G_ZLIB_COMPRESSOR_FORMAT_GZIP);
-        gz_file_inputstream = g_file_read(gz_file, cancellable, &error);
-        stream_data         = g_converter_input_stream_new(G_INPUT_STREAM(gz_file_inputstream),
-                                                           G_CONVERTER(decompressor));
-
-        // Delete old xml file if it exists
-        if (g_file_query_exists(xml_file, cancellable)) {
-            g_debug("Deleting old metadata file");
-            g_file_delete(xml_file, cancellable, &error);
-        }
-
-        // Create new file to store uncompressed data
-        g_debug("Creating new metadata file");
-        xml_file = g_file_new_for_path(metadata_filename);
-        xml_file_iostream =
-            g_file_create_readwrite(xml_file, G_FILE_CREATE_NONE, cancellable, &error);
-        xml_outputstream = g_io_stream_get_output_stream(G_IO_STREAM(xml_file_iostream));
-
-        // Read from gz and write to xml
-        gssize read_count   = 1;
-        guint8 buffer[1024] = {0};
-
-        g_debug("Writing metadata");
-        while (read_count) {
-            read_count = g_input_stream_read(stream_data, buffer, 1024, cancellable, &error);
-            g_output_stream_write(xml_outputstream, buffer, 1024, cancellable, &error);
-        }
-
-        // Delete gz file
-        /* g_file_delete(gz_file, cancellable, &error); */
-
-        // Build silo
-        add_apps_from_metadata_file(self, gz_file, cancellable, &error);
-
-        g_task_return_boolean(task, TRUE);
-    }
 }
 
 /*
@@ -167,6 +158,8 @@ add_apps_from_metadata_file(GsPluginVanillaMeta *self,
     g_autoptr(XbBuilder) builder      = xb_builder_new();
     g_autoptr(XbBuilderSource) source = xb_builder_source_new();
     g_autoptr(GFile) silo_file        = g_file_new_for_path(metadata_silo_filename);
+
+    g_debug("Loading app silo");
 
     // Add current locales
     for (guint i = 0; locales[i] != NULL; i++)
@@ -221,7 +214,13 @@ gs_plugin_vanilla_meta_refresh_metadata_finish(GsPlugin *plugin,
 /*                                        GAsyncReadyCallback callback, */
 /*                                        gpointer user_data) */
 /* { */
+/*     g_autoptr(GTask) task = NULL; */
 /**/
+/*     task = */
+/*         gs_plugin_list_apps_data_new_task(plugin, query, flags, cancellable, callback,
+ * user_data); */
+/**/
+/*     g_task_set_source_tag(task, gs_plugin_vanilla_meta_list_apps_async); */
 /* } */
 /**/
 /* static GsAppList * */
@@ -233,8 +232,13 @@ gs_plugin_vanilla_meta_refresh_metadata_finish(GsPlugin *plugin,
 static void
 gs_plugin_vanilla_meta_class_init(GsPluginVanillaMetaClass *klass)
 {
+    GObjectClass *object_class  = G_OBJECT_CLASS(klass);
     GsPluginClass *plugin_class = GS_PLUGIN_CLASS(klass);
 
+    object_class->dispose = gs_plugin_vanilla_meta_dispose;
+
+    plugin_class->setup_async             = gs_plugin_vanilla_meta_setup_async;
+    plugin_class->setup_finish            = gs_plugin_vanilla_meta_setup_finish;
     plugin_class->refresh_metadata_async  = gs_plugin_vanilla_meta_refresh_metadata_async;
     plugin_class->refresh_metadata_finish = gs_plugin_vanilla_meta_refresh_metadata_finish;
     /* plugin_class->list_apps_async         = gs_plugin_vanilla_meta_list_apps_async; */
